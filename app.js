9f510c82c4f24cf6d7ab0feb69dec356146fe1de
Sümeyye Akkaş <akkasumeyye@hotmail.com>
2023-01-08 02:00:00 +0300

    admin panel userdata cekildi tablolandı

--- app.js
+++ app.js
@@ -1,53 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3030;
-const middleware = require('./middleware');
-const path = require('path');
-const bodyParser = require('body-parser');
-const mongoose = require('./database');
-const session = require('express-session');
+const port = process.env.PORT || 3035;
+const middleware = require("./middleware");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("./database");
+const session = require("express-session");
 const cookieParser = require("cookie-parser");
 
-
-const server = app.listen(port, () => console.log("Example app listening on port !"+port));
+const server = app.listen(port, () =>
+  console.log("Example app listening on port !" + port)
+);
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
-
-app.use(session({
-    secret: 'keyboard cat',
+app.use(
+  session({
+    secret: "keyboard cat",
     resave: true,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
 //Routes
-const loginRoute = require('./routes/loginRoutes.js');
-const registerRoute = require('./routes/registerRoutes.js');
-const logoutRoute = require('./routes/logout.js');
-const adminRoute = require('./routes/adminRoutes.js');
+const loginRoute = require("./routes/loginRoutes.js");
+const registerRoute = require("./routes/registerRoutes.js");
+const logoutRoute = require("./routes/logout.js");
+const adminRoute = require("./routes/adminRoutes.js");
 
 //Api Routes
-const postsApiRoute = require('./routes/api/posts');
-const { JwtAuth } = require('./authJwt');
+const postsApiRoute = require("./routes/api/posts");
+const { JwtAuth } = require("./authJwt");
 
-app.use('/api/posts', ['admin'], postsApiRoute);
+app.use("/api/posts", postsApiRoute);
 
-app.use('/login', loginRoute);
-app.use('/register', registerRoute);
-app.use('/logout', logoutRoute);
-app.use('/admin',  middleware.requireLogin, JwtAuth, adminRoute);
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
+app.use("/logout", logoutRoute);
+app.use("/admin", middleware.requireLogin, JwtAuth, adminRoute);
 
-app.get('/', middleware.requireLogin, JwtAuth, (req, res, next) => { 
-
-   var payload = {
+app.get("/", middleware.requireLogin, JwtAuth, (req, res, next) => {
+  var payload = {
     pageTitle: "Home",
-    userLoggedIn: req.session.user
-   }
+    userLoggedIn: req.session.user,
+  };
 
-    res.status(200).render("home", payload); 
+  res.status(200).render("home", payload);
 });
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
 
 const User = require("../schemas/UserSchema");
 
@@ -11,10 +10,27 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-router.get("/", (req, res, next) => { 
-    res.send("Hello World!");
+
+router.get("/", (req, res) => {
+  User.find()
+    .then((results) =>  {
+    //   console.log(results);
+      res.render("admin", { users: results })
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(400);
+    });
 });
 
-
+router.post("/", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    console.log(req.body);
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 module.exports = router;
